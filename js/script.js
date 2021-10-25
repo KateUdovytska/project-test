b297fa6e346ca684e4d4c98f81b94ab53a4a9adb
Kate <ketrina113@gmail.com>
2021-10-25 14:14:29 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,38 @@
+"use strict";
+let number = 5;
+const leftBorderWidth = 1;
+
+let number2 = 4.6;
+const persone = 'Alex';
+const bool = true;
+
+const obj = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+console.log(obj.name);
+
+let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp'];
+console.log(arr[2]);
+
+
+// alert('Hello');
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = prompt("Are you 18?", "18");
+// console.log(answer);
+
+// const answers = [];
+
+// answers[0] = prompt("What is your name", "");
+// answers[1] = prompt("What is your surame", "");
+// answers[2] = prompt("What is your age", "");
+
+// document.write(answers);
+
+const category = 'toys';
+
+console.log('https://someurl.com/'+ category);
