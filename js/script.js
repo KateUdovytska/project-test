5d6476dcd3654db59edbdb579ba03b9158bfe21d
kate <ketrina113@gmail.com>
2022-03-18 00:25:27 +0200

    lesson2

--- js/script.js
+++ js/script.js
@@ -1,22 +1,4 @@
 "use strict";
-/* Задание на урок:
-1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
-'Сколько фильмов вы уже посмотрели?'
-2) Создать объект personalMovieDB и в него поместить такие свойства:
-    - count - сюда передается ответ на первый вопрос
-    - movies - в это свойство поместить пустой объект
-    - actors - тоже поместить пустой объект
-    - genres - сюда поместить пустой массив
-    - privat - в это свойство поместить boolean(логическое) значение false
-3) Задайте пользователю по два раза вопросы:
-    - 'Один из последних просмотренных фильмов?'
-    - 'На сколько оцените его?'
-Ответы стоит поместить в отдельные переменные
-Записать ответы в объект movies в формате: 
-    movies: {
-        'logan': '8.1'
-    }
-Проверить, чтобы все работало без ошибок в консоли */
 
 const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?");
 const personalMovieDB = {
@@ -26,12 +8,28 @@ const personalMovieDB = {
     genres: [],
     privat: false
 };
-
-let a = prompt("Один из просмотренных фильов?");
-let b = prompt("На сколько оцените его?");
-let c = prompt("Один из просмотренных фильов?");
-let d = prompt("На сколько оцените его?");
-
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
 console.log(personalMovieDB);
+
+
+for(let i = 0; i < 2; i++){
+    let a = prompt("Один из просмотренных фильмов?");
+    let b = prompt("На сколько оцените его?");
+
+    if(a != null && b != null && a != '' && b !='' && a.length < 50){
+        personalMovieDB.movies[a] = b;
+    }else {
+        i--;
+    }
+
+}
+console.log(personalMovieDB);
+
+if (personalMovieDB.count < 10){
+    console.log("Просмотрено довольно мало фильмов");
+}else if(personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+    console.log("Вы классический зритель");
+}else if(personalMovieDB.count <= 30){
+    console.log("Вы киноман");
+}else{
+    console.log("Произошла ошибка");
+}
